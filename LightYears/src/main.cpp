1ca1c7effd2be8b0daaa72e384bb8db4609282c9
emirhan <emirhangnn0607@gmail.com>
2026-05-30 22:30:12 +0300

    new: sfml setup

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
+#include "SFML/Graphics.hpp"
 
 int main(int argc, char *argv[]) {
+    sf::RenderWindow renderWindow{sf::VideoMode(sf::VideoMode({100, 100})), "My Window"};
     std::cout << "Hello World!" << std::endl;
 }
---
+++ cmake-build-debug/_deps/sheenbidi-build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Software/C++/LightYears/cmake-build-debug/_deps/sheenbidi-src
+# Build directory: C:/Software/C++/LightYears/cmake-build-debug/_deps/sheenbidi-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
