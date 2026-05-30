efc54564388d11441d6449cde38063d9c28593b3
emirhan <emirhangnn0607@gmail.com>
2026-05-30 23:33:35 +0300

    new:: application system base

---
+++ LightYears/include/framework/Application.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+
+namespace ly {
+    class Application {
+    public:
+        Application();
+        void Run();
+
+    private:
+        sf::RenderWindow m_Window;
+    };
+}
--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
-#include "SFML/Graphics.hpp"
+#include "framework/Application.h"
+#include <memory>
 
 int main(int argc, char *argv[]) {
-    sf::RenderWindow renderWindow{sf::VideoMode(sf::VideoMode({100, 100})), "My Window"};
-    std::cout << "Hello World!" << std::endl;
+    std::unique_ptr<ly::Application> app = std::make_unique<ly::Application>();
+    app->Run();
 }
